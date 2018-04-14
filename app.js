08c86e29651c61aa58443e34524f7dacacae7c0a
Jasenpan1987 <jasenpan@gmail.com>
2018-04-14 17:03:54 +1000

    curd api complete

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const static = require
 
 const keys = require("./config");
 
@@ -10,18 +12,27 @@ const routeConfig = require("./routes/apis");
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
 mongoose.connect(keys.MONGO_URI, () => {
   console.log("mongo is connected");
 });
 
 routeConfig(app);
 
-app.get('/', (req, res) => {
-  return res.send("hello world");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    return res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html")
+    );
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("server listen to localhost:" + PORT);
+  console.log("server listen to localhost: " + PORT);
 });
--- routes/apis.js
+++ routes/apis.js
@@ -6,20 +6,73 @@ const Posts = mongoose.model("posts");
 module.exports = app => {
   // get all posts /post/*
   app.get("/api/posts", async (req, res) => {
-    const posts = await Posts.find({});
-    return res.send(posts);
+    try {
+      const posts = await Posts.find({});
+      return res.send(posts);
+    } catch(error) {
+      return res.send({ error });
+    }
   });
 
-var counter = 0;
   // create posts
-  app.get('/api/new', async (req, res) => {
+ app.post('/api/post', async (req, res) => {
+   const { title, body, category } = req.body;
     const newPost = new Posts({
-      title: `title ${++counter}`,
-      body: `body ${counter}`,
-      category: "random"
+      title,
+      body,
+      category
     });
 
-    const result = await newPost.save()
-    return res.send({ result });
+    try {
+      const result = await newPost.save()
+      return res.send({ status: "success", ...result });
+    } catch (error) {
+      return res.send({ error });
+    }
+  });
+
+  // get post by id
+  app.get('/api/post/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const post = await Posts.findById(id);
+      return res.send(post);
+    } catch (error) {
+      return res.send({ error });
+    }
+  });
+  
+  // get post by query
+  app.get('/api/post', async (req, res) => {
+    const category = req.query.category;
+    try {
+      const post = await Posts.find({ category });
+      return res.send(post);
+    } catch (error) {
+      return res.send({ error });
+    }
+  });
+
+  // update post
+  app.put('/api/post/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, body, category } = req.body
+    try {
+      const result = await Posts.findByIdAndUpdate(id, { title, body, category });
+      return res.send({ status: "success", ...result });
+    } catch (error) {
+      return res.send({ error });
+    }
+  });
+
+  // delete post
+  app.delete('/api/post/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Posts.findByIdAndRemove(id);
+      return res.send({ status: "success" });
+    } catch (error) {
+      return res.send({ error });
+    }
   });
 };
